3c47749b50de62c385f541d73395fc72d4a7d19b
alexeikasyanik <lyohakasianik@gmail.com>
2019-08-11 23:28:36 +0300

    3 xml

---
+++ app/src/main/java/com/gmail/lyohakasianik/leaguestatistics/StartMenu.kt
@@ -0,0 +1,20 @@
+package com.gmail.lyohakasianik.leaguestatistics
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import kotlinx.android.synthetic.main.layout_menu_start.*
+
+class StartMenu : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_menu_start)
+
+        buttonShowFreeChampions.setOnClickListener {
+            val intent = Intent(this, ListFreeChampionsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
